00922ec16291d87bcd0654a4081a0b60189bfa1c
Prince Kumar Ram <132006575+princekumarram@users.noreply.github.com>
2026-02-05 08:54:05 +0530

    commit

--- LinkedList/Medium_Problem_of_LL/p10_SortLL.java
+++ LinkedList/Medium_Problem_of_LL/p10_SortLL.java
@@ -20,28 +20,85 @@ class Node {
 
 public class p10_SortLL {
 
-    static Node sortLL(Node head) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        Node temp = head;
+    // static Node sortLL(Node head) {
+    // ArrayList<Integer> arr = new ArrayList<>();
+    // Node temp = head;
 
-        while (temp != null) {
-            arr.add(temp.data);
-            temp = temp.next;
-        }
+    // while (temp != null) {
+    // arr.add(temp.data);
+    // temp = temp.next;
+    // }
 
-        Collections.sort(arr);
+    // Collections.sort(arr);
 
-        temp = head;
+    // temp = head;
 
-        for (int i = 0; i < arr.size(); i++) {
-            temp.data = arr.get(i);
-            temp = temp.next;
-        }
-        return head;
-    }
+    // for (int i = 0; i < arr.size(); i++) {
+    // temp.data = arr.get(i);
+    // temp = temp.next;
+    // }
+    // return head;
+    // }
 
     static Node mergeTwoSortedLinkedLists(Node list1, Node list2) {
 
+        Node dummyNode = new Node(-1, null);
+        Node temp = dummyNode;
+
+        while (list1 != null && list2 != null) {
+            if (list1.data <= list2.data) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        // Attach remaining nodes
+        if (list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+
+        return dummyNode.next;
+    }
+
+    public static Node findMiddle(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static Node sortLL(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node middle = findMiddle(head);
+
+        // Split into two halves
+        Node right = middle.next;
+        middle.next = null;
+        Node left = head;
+
+        // Recursively sort both halves
+        left = sortLL(left);
+        right = sortLL(right);
+
+        return mergeTwoSortedLinkedLists(left, right);
     }
 
     static void printLinkedList(Node head) {
