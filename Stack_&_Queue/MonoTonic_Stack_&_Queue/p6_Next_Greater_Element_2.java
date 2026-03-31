b76903f566474a664d90ce471bfece0a442f8916
Prince Kumar Ram <132006575+princekumarram@users.noreply.github.com>
2026-03-31 17:44:32 +0000

    23-03-2026

--- Stack_&_Queue/MonoTonic_Stack_&_Queue/p6_Next_Greater_Element_2.java
+++ Stack_&_Queue/MonoTonic_Stack_&_Queue/p6_Next_Greater_Element_2.java
@@ -27,12 +27,46 @@ class p6_Next_Greater_Element_2{
 
         return ans;
     }
+
+    //Optimal
+    public static int[]  nextGreaterElements2(int[] arr) {
+      int n=arr.length;
+      int[] ans=new int[n];
+
+      Stack<Integer>st=new Stack<>();
+
+ // Traverse the array twice in reverse to simulate circular array
+      for(int i=2*n-1;i>=0;i--){
+        int ind=i%n;
+        int currEle=arr[ind];
+
+  // Pop all elements from stack that are less than or equal to current
+        while(!st.isEmpty() && st.peek()<=currEle){
+            st.pop();
+        }
+
+
+            // Only fill result in the first pass (i < n)
+        if(i<n){
+            if(st.isEmpty()){
+                ans[ind]=-1;
+            }else{
+                ans[ind]=st.peek();
+            }
+        }
+        st.push(currEle);
+      }
+
+         return ans;
+    }
+
        public static void main(String[] args) {
         int[] arr = {5, 7, 1, 7, 6, 0};
  
 
         // Call function
-        int[] ans = nextGreaterElements(arr);
+        // int[] ans = nextGreaterElements(arr);
+        int []ans= nextGreaterElements2(arr);
 
         System.out.println("The next greater elements are: ");
         for (int num : ans) {
